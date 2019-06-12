ba82224b0a2d83c6a8cc1fb3b4a5fe07ec975af0
TecJP <tec.joao.freitas@gmail.com>
2019-06-12 17:50:22 -0300

    remoção de códigos excessivos para deixar o gráfico mais clean

--- _Side_Navbar/js/script.js
+++ _Side_Navbar/js/script.js
@@ -7,34 +7,24 @@ var myChart = new Chart(ctx, {
     labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
     datasets: [{
       label: 'Atividades Realizadas',
-      data: [2, 3, 4, 4, 5, 6],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
+      data: [1.5, 3, 2, 4.2, 2.9, 1.2],
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 4,
+      pointBackgroundColor: 'rgba(255, 99, 132, 1)'
     }]
   },
   options: {
-    responsive: true,
     scales: {
       yAxes: [{
         ticks: {
-          beginAtZero: true
+          beginAtZero: false
         }
       }]
+    }, 
+    legend: {
+      display: false,
     }
   }
 });
